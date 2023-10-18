22427067efc566e1fede22ee3b545f4fbc737489
Josuejosue1999 <j.biaou@alustudent.com>
2023-10-18 20:44:27 +0200

    My Resume

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    // Animation de l'élément h3
+    $(".home-content h3").addClass("animate__animated animate__fadeInDown");
+
+    // Animation de l'élément h1 avec la classe "span"
+    $(".home-content h1").addClass("animate__animated animate__fadeInDown");
+    $(".home-content h1 span").addClass("animate__animated animate__fadeInDown");
+
+    // Animation de l'élément p
+    $(".home-content p").addClass("animate__animated animate__fadeIn");
+});
